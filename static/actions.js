18ef263be845338dbf9c33b2595ef64acf092a22
tromoSM <tromosm7@gmail.com>
2026-02-21 02:19:59 +0530

    Update message prompt to use async function

--- static/actions.js
+++ static/actions.js
@@ -1,7 +1,11 @@
 window.addEventListener("DOMContentLoaded",function(){
     document.querySelector('[tromoSM="scrn"]').addEventListener("click",printscr)
-    document.querySelector('[tromoSM="mess"]').addEventListener("click",function(){
-        let temp$$pr$$mes=prompt("Send a message to the host")
+    document.querySelector('[tromoSM="mess"]').addEventListener("click",async function(){
+        let temp$$pr$$mes=await iOSprompt("Send a message to the host")
         message(temp$$pr$$mes)
+    })
+    document.querySelector('[tromoSM="obfi"]').addEventListener('click',function(){
+    document.querySelector('[tromoSM="mainvid"]').toggleAttribute('cover')
+    document.querySelector('[tromoSM="mainvid"]').toggleAttribute('scale')
     })
 })
