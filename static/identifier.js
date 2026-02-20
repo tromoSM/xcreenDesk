18cad13859be1519896e41091b5cba96d1117f39
tromoSM <tromosm7@gmail.com>
2026-02-21 02:18:51 +0530

    Handle username prompt with error fallback

--- static/identifier.js
+++ static/identifier.js
@@ -1,7 +1,15 @@
 window.addEventListener("DOMContentLoaded",async function(){
     if(!localStorage.getItem("username")){
-     let temp$$pr$usr=prompt("choose an username")
-    await localStorage.setItem("username",temp$$pr$usr.trim())    
+    try{
+      let temp$$pr$usr=await iOSprompt("choose an username")
+     await localStorage.setItem("username",temp$$pr$usr.trim())    
+    }
+    catch(dih){
+     errorr(dih)
+      let temp$$pr$usr=prompt("choose an username")
+     await localStorage.setItem("username",temp$$pr$usr.trim())    
+    }
+    
     }
     window.username=localStorage.getItem("username")
 })
