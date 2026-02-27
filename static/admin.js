12b3837ca82bc8d61632b789d5a3a1ff6aae90b6
tromoSM <tromosm7@gmail.com>
2026-02-27 07:18:59 +0530

    add license info to admin.js

--- static/admin.js
+++ static/admin.js
@@ -1,7 +1,33 @@
+/*
+Copyright 2026 tromoSM
+https://github.com/tromoSM/xcreenDesk
+static/admin.js
+v1 GUI
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 window.addEventListener("DOMContentLoaded",function(){
     loader("please wait",'s')
      window.addEventListener('pywebviewready',async function(){
      loader("sybau",'idk cro')
+    })
+    const sdih=io()
+    let all$$banned$$=[]
+    sdih.on("autoban",function(u){
+      all$$banned$$.push(u)
+    })
+    sdih.on("unban",function(u){
+      all$$banned$$.splice(all$$banned$$.indexOf(u))
     })
     let temp$main$ind=document.querySelector('mainindicate')
     temp$main$ind.setAttribute("share",'fuhnaw')
@@ -19,7 +45,6 @@ window.addEventListener("DOMContentLoaded",function(){
          temp$main$ind.setAttribute("share",'fuhnaw')
         }
     })
-    const sdih=io()
     sdih.on("imim_main",function(imim){
      window.usg=imim.length/(1024*1024)
       document.querySelector('[tromoSM="mainvid"]').addEventListener('click',function(){
@@ -102,5 +127,38 @@ window.addEventListener("DOMContentLoaded",function(){
     })
     document.querySelector('[tromoSM="nomess"]').addEventListener('click',async function(){
      await window.pywebview.api.chngmsg('f')
+    })
+    let constin=''
+    function refreshpla(){
+      constin=document.querySelector('[inban="appear"]').value
+      if(all$$banned$$.includes(constin)){
+        document.querySelector('[unor="ban"]').innerHTML='Unban'
+      }
+      else{
+        document.querySelector('[unor="ban"]').innerHTML='Ban'
+
+      }
+    }
+
+    document.querySelector('[inban="appear"]').addEventListener("input",function(){refreshpla()})
+    document.querySelector('[unor="ban"]').addEventListener("click",async function(){
+      constin=document.querySelector('[inban="appear"]').value.trim()
+      if(!constin){
+        return
+      }
+      if(all$$banned$$.includes(constin)){
+        await window.pywebview.api.BAN(['u',constin])
+        all$$banned$$=all$$banned$$.filter(dihdih=>dihdih!==constin)
+      }
+      else{
+        await window.pywebview.api.BAN(['b',constin])
+        all$$banned$$.push(constin)
+      }
+      refreshpla()
+    })
+    document.querySelector('[inban]').addEventListener('keydown',function(dih){
+      if(dih.key=="Enter"){
+        document.querySelector('[unor="ban"]').click()
+      }
     })
 })
