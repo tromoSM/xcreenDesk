afc2600809e7b80fbf963a02bc26e9be8cd2dd15
tromoSM <tromosm7@gmail.com>
2026-02-27 07:33:12 +0530

    add license info to main.js

--- static/main.js
+++ static/main.js
@@ -1,3 +1,21 @@
+/*
+Copyright 2026 tromoSM
+https://github.com/tromoSM/xcreenDesk
+static/main.js
+v1 GUI/CLI
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 window.addEventListener("DOMContentLoaded",function(){
   window.wait=function(dih){
         return new Promise(resolve=>setTimeout(resolve,dih))
@@ -7,7 +25,20 @@ window.addEventListener("DOMContentLoaded",function(){
   return(/Mobi|iPhone|iPad|iPod|BlackBerry|Android|IEMobile|Opera Mini/i.test(navigator.userAgent)||(window.innerWidth<=768&&navigator.maxTouchPoints>0))
   }
   sdih.emit("dih",[username,returndih()])
-    sdih.on("imim_main",function(imim){
+  sdih.on("autoban",async function(dih){
+    if(dih.trim()==username.trim()){
+      await document.documentElement.remove()
+      await wait(300)
+      alert("you are banned from this session")
+    }
+  })
+  sdih.on("unban",function(u){
+    if(u.trim()==username.trim()){
+     window.location.reload()
+    }
+  })
+  
+  sdih.on("imim_main",function(imim){
      window.usg=imim.length/(1024*1024)
       document.querySelector('[tromoSM="mainvid"]').addEventListener('click',function(){
       })
